856bb724d13f3e66d6785aff0be28f9a63c0384f
radengugi <radengugi@gmail.com>
2021-07-01 13:20:03 +0700

    BE-gugi-03 : Keeplogin, Komentar, CRUD News

--- routers/newsRouter.js
+++ routers/newsRouter.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const { readToken } = require('../config')
 const { newsController } = require('../controllers')
 
 router.get('/get-news', newsController.getNews)
@@ -6,4 +7,9 @@ router.post('/add', newsController.addNews)
 router.patch('/update', newsController.updateNews)
 router.delete('/delete/:idnews', newsController.deleteNews)
 router.patch('/update-view', newsController.updateView)
+
+// Komentar
+router.get('/get-komentar', newsController.getKomentar)
+router.post('/add-komentar', newsController.addKomentar)
+
 module.exports = router
